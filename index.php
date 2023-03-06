d9787e6cb4be06b3e852343dcbfff322999a89f7
MarikArh <72720309+MarikArh@users.noreply.github.com>
2023-03-06 04:47:52 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,76 +1,127 @@
 <!DOCTYPE html>
-<html>
-<head>
-    <title>Решение лабиринта</title>
-    <style>
-       .frame {
-        border: 10px solid black;
-       }
-  </style>
-</head>
-<body>
-    <header>
-        <h1>Решатель Антон</h1>
-    </header>
-    
-    <form method="post" action="">
-        <label for="arr">Введите двумерный массив, вводите значения через запяту, каждую строку на новой строке:</label>
-        <br>
-        <textarea name="arr" id="arr" rows="10" cols="20"></textarea>
-        <br>
-        <label for="x1">Введите координату x1:</label>
-        <input type="number" name="x1" id="x1">
-        
-        <label for="y1">Введите координату y1:</label>
-        <input type="number" name="y1" id="y1">
-        <br>
-        <label for="x2">Введите координату x2:</label>
-        <input type="number" name="x2" id="x2">
-        
-        <label for="y2">Введите координату y2:</label>
-        <input type="number" name="y2" id="y2">
-        <br>
-        <input type="submit" value="Выполнить">
-        <br>
-    </form>
+<html data-bs-theme="dark">
+    <head>
+        <title>Решение лабиринта</title>
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    </head>
+    <body>
+        <header>
+            <nav class="navbar navbar-expand-lg navbar-dark bg-primary bg-gradient">
+                <a class="navbar-brand ml-3" href="#">
+                    <h1>Решатель Антон v1.1</h1>
+                </a>
+            </nav>
+        </header>
+        <div class="container-fluid">
+            <div class="row p-3">
+                <div class="col-md-4 p-3 content ">
+                    <form method="post" action="">
+                        <label class="form-label" for="arr">
+                            <h4>Введите двумерный массив, элементы можно не разделять, или разделять следующими знаками:<br>"(" "[" "," "пробел". <br>
+                            Строки необходимо разделять с помощью следующих символов: <br>
+                            "]" ")" "\n" - перенос строки. </h4>
+                        </label>
+                        <br>
+                        <textarea class="form-control" name="arr" id="arr" rows="10" cols="20"></textarea>
+                        <br>
+                        <label class="form-label" for="y1">
+                            <p class="text-justify">Введите координату X Начальной точки:</p>
+                        </label>
+                        <input type="text" class="form-control" name="y1" id="y1">
+                        <br>
+                        <label class="form-label" for="x1">
+                            <p class="text-justify">Введите координату Y Начальной точки:</p>
+                        </label>
+                        <input type="text" class="form-control" name="x1" id="x1">
+                        <br>
+                        <label class="form-label" for="y2">
+                            <p class="text-justify">Введите координату X Конечной точки:</p>
+                        </label>
+                        <input type="text" class="form-control" name="y2" id="y2">
+                        <br>
+                        <label class="form-label" for="x2">
+                            <p class="text-justify">Введите координату Y Конечной точки:</p>
+                        </label>
+                        <input type="text" class="form-control" name="x2" id="x2">
+                        <br>
+                        <input type="submit" class="form-control" value="Выполнить">
+                        <br>
+                    </form>
+                    <p>Большую часть работы занимает генерация картинки,при особо большом лабиринте возможно долгое ожидание  </p>
+                    <p>
+                        Сложность данного алгоритма - ~O(n*n), - просмотреть каждую ячейку.
+                        Короче сделать невозможно по моему мнению, ведь не просмотрев все пути, невозможно утверждать что кратчайший путь найден.
 
-    <?php
-    require_once("mazeUtils.php");
-    require_once("maze.php");
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        // Получаем массив из POST-запроса
-        $input = $_POST['arr'];
-        if ($input!==''){
-            $rows = explode("\n", $input);
-            $output = array();
-            foreach ($rows as $row) {
-                $row = trim($row);
-                $elements = explode(",", $row);
-                $output_row = array();
-                foreach ($elements as $element) {
-                    $output_row[] = intval(trim($element));
-                }
-                $output[] = $output_row;
-            } 
-            $START=new Cell(intval($_POST['x1']),intval($_POST['y1']));
-            $END=new Cell(intval($_POST['x2']),intval($_POST['y2']));
-            if ($output[$START->x][$START->y]!==0 && $output[$END->x][$END->y]!==0 ){
-                $maze=new Maze($output,$START,$END);
-                $maze->solution();
-                if ($maze->is_solved()){
-                    $maze->get_way_img(100);
-                    echo "<img src='solution.png' class='frame'>";
-                    echo "<br>";
-                    echo "Длина пути: ".$maze->get_length();
-                }else{
-                    echo "Нет ответа";
-                }
-            }else{
-                    echo "Нет варианта";
-            }
-            }
-        }
-    ?>
+                        Суть моего алгоритма в том, что он считает кратчайший путь в каждую развилку, если находит уже просчитанную развилку и видит, что может короче - считает из неё заново.
+                    </p>
+                </div>
+                <div class="col-md-2"></div>
+                <div class=" col-md-6 p-3 content"> <?php
+                    require_once "mazeUtils.php";
+                    require_once "maze.php";
+                    if ($_SERVER["REQUEST_METHOD"] === "POST") {
+                        $input = $_POST["arr"];
+                        if ($input !== "") {
+                            $input=str_replace("(",'',$input);
+                            $input=str_replace("(",'',$input);
+                            $input=str_replace("[",'',$input);
+                            $input=str_replace(",",'',$input);
+                            $input=str_replace(" ",'',$input);
+                            if( strpos($input,"\n")){
+                                $input=str_replace(")",'',$input);
+                                $input=str_replace("]",'',$input);
+                                $rows = explode("\n", $input);
+                            }elseif(strpos($input,"]")){
+                                $input=str_replace(")",'',$input);
+                                $rows = explode("]", $input);
+                            }elseif(strpos($input,")")){
+                                $input=str_replace("]",'',$input);
+                                $rows = explode(")", $input);
+                            }
+                            $output = [];
 
-</body>
+                            foreach ($rows as $row) {
+                                $row = trim($row);
+                                if ($row !== "") {
+                                    $output_row = [];
+                                    $elements=str_split($row);
+                                    foreach ($elements as $element) {
+                                        if ($element !== "") {
+                                            $output_row[] = intval(trim($element));
+                                        }
+                                    }
+                                    $output[] = $output_row;
+                                }
+                            }
+                            $START = new Cell(intval($_POST["x1"]), intval($_POST["y1"]));
+                            $END = new Cell(intval($_POST["x2"]), intval($_POST["y2"]));
+                            if (
+                                $output[$START->x][$START->y] !== 0 &&
+                                $output[$END->x][$END->y] !== 0
+                            ) {
+                                $maze = new Maze($output, $START, $END);
+                                $maze->solution();
+                                if ($maze->is_solved()) {
+                                    //$maze->get_way_img(128);
+                                    echo "<h4 class='text-center'>Длина пути: " .$maze->get_length() ."</h4>";
+                                    echo "<h6>Ответ вида r-вправо,d-вниз,l-влево,u-вверх :</h6><h6>[".$maze->compass_map."]</h6>";
+                                    echo "<img src='img/solution.png' class='mx-auto d-block img-fluid frame'><br>";
+                                } else {
+                                    echo "<h4>Нет ответа</h4>";
+                                }
+                            } else {
+                                echo "<h4>Нет ответа</h4>";
+                            }
+                        }
+                    }
+                ?> </div>
+            </div>
+        </div>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    </body>
 </html>
--- maze.php
+++ maze.php
@@ -1,207 +1,249 @@
-<?php 
+<?php
 
-require_once("mazeUtils.php");
+require_once "mazeUtils.php";
 
-class Maze {
-  public $WALL = 0;
-  public $BLANK = 1;
-  public $maze;
-  public $START;
-  public $END;
-  public $correct_way;
-  public $LENGTH;
-  public $HEIGHT;
-  public $paths;
-
-  public function __construct($maze,Cell $start,Cell $end) {
-    $this->WALL = 0;
-    $this->BLANK = 1;
-    $this->maze = $maze;
-    $this->START = $start;
-    $this->END = $end;
-    $this->LENGTH = count($this->maze[0]);
-    $this->HEIGHT = count($this->maze);
-    $this->paths = array();
-    for ($i=0; $i<$this->HEIGHT; $i++) {
-      $this->paths[$i] = array();
-    }
-    $this->paths[$this->START->x][$this->START->y] = $this->maze[$this->START->x][$this->START->y];
-    $this->correct_way = $this->paths;
-    $this->stack = new Stack($this->START, $this->START);
-  }
-
-  public function look($dirrection, $is_answer=0) {
-    $x = 0;
-    $y = 0;
-    if ($dirrection == 'up') {
-      $x = -1;
-    } elseif ($dirrection == 'right') {
-      $y = 1;
-    } elseif ($dirrection == 'down') {
-      $x = 1;
-    } elseif ($dirrection == 'left') {
-      $y = -1;
-    }
-    $current = $this->stack->curr;
-    $way_x = $current->x + $x;
-    $way_y = $current->y + $y;
-    $way = new Cell($way_x, $way_y);
-
-    if ((-1 < $way_x && $way_x < $this->HEIGHT) && (-1 < $way_y && $way_y < $this->LENGTH) && $way != $this->stack->last) {
-      if ($this->maze[$way_x][$way_y] != 0 ) {
-        if (array_key_exists($way_y, $this->paths[$way_x])) {
-          if ($this->paths[$way_x][$way_y] > ($this->paths[$current->x][$current->y] + $this->maze[$way_x][$way_y])) {
-            $this->paths[$way_x][$way_y] = $this->paths[$current->x][$current->y] + $this->maze[$way_x][$way_y];
-            return $way;
-          }
-        } else {
-          $this->paths[$way_x][$way_y] = $this->paths[$current->x][$current->y] + $this->maze[$way_x][$way_y];
-          return $way;
-        }
-      }
-    }
-    return false;
-  }
-
-  public function observe() {
-    $vars = array();
-    $up = $this->look('up');
-    $right = $this->look('right');
-    $down = $this->look('down');
-    $left = $this->look('left');
-
-    if ($up) {
-      array_push($vars, $up);
-    }
-    if ($right) {
-      array_push($vars, $right);
-    }
-    if ($down) {
-      array_push($vars, $down);
-    }
-    if ($left) {
-      array_push($vars, $left);
-    }
-    return $vars;
-  }
-  public function get_length(){
-    $label = $this->END;
-    return $this->correct_way[$label->x][$label->y];
-  }
-  public function pathfind()
+class Maze
+{
+    public $maze;
+    public $START;
+    public $END;
+    public $correct_way;
+    public $LENGTH;
+    public $HEIGHT;
+    public $paths;
+    public $compass_map;
+    public $way_map;
+    public function __construct($maze, Cell $start, Cell $end)
     {
-    while ($this->stack->getLength() !== 0) {
-        $vars = $this->observe();
-        if (count($vars) === 0) {
-            $this->stack->remove();
-        } elseif (count($vars) === 1) {
-            $this->stack->moveCell($vars[0]);
+        $this->maze = $maze;
+        $this->START = $start;
+        $this->END = $end;
+        $this->LENGTH = count($this->maze[0]);
+        $this->HEIGHT = count($this->maze);
+        $this->paths = [];
+        for ($i = 0; $i < $this->HEIGHT; $i++) {
+            $this->paths[$i] = [];
+        }
+        $this->paths[$this->START->x][$this->START->y] =
+            $this->maze[$this->START->x][$this->START->y];
+        $this->correct_way = $this->paths;
+        $this->stack = new Stack($this->START, $this->START);
+    }
+
+    public function look($dirrection, $is_answer = 0)
+    {
+        $x = 0;
+        $y = 0;
+        if ($dirrection == "up") {
+            $x = -1;
+        } elseif ($dirrection == "right") {
+            $y = 1;
+        } elseif ($dirrection == "down") {
+            $x = 1;
+        } elseif ($dirrection == "left") {
+            $y = -1;
+        }
+        $current = $this->stack->curr;
+        $way_x = $current->x + $x;
+        $way_y = $current->y + $y;
+        $way = new Cell($way_x, $way_y);
+
+        if (
+            -1 < $way_x &&
+            $way_x < $this->HEIGHT &&
+            (-1 < $way_y && $way_y < $this->LENGTH) &&
+            $way != $this->stack->last
+        ) {
+            if ($this->maze[$way_x][$way_y] != 0) {
+                if (array_key_exists($way_y, $this->paths[$way_x])) {
+                    if (
+                        $this->paths[$way_x][$way_y] >
+                        $this->paths[$current->x][$current->y] +
+                            $this->maze[$way_x][$way_y]
+                    ) {
+                        $this->paths[$way_x][$way_y] =
+                            $this->paths[$current->x][$current->y] +
+                            $this->maze[$way_x][$way_y];
+                        return $way;
+                    }
+                } else {
+                    $this->paths[$way_x][$way_y] =
+                        $this->paths[$current->x][$current->y] +
+                        $this->maze[$way_x][$way_y];
+                    return $way;
+                }
+            }
+        }
+        return false;
+    }
+
+    public function observe()
+    {
+        $vars = [];
+        $up = $this->look("up");
+        $right = $this->look("right");
+        $down = $this->look("down");
+        $left = $this->look("left");
+
+        if ($up) {
+            array_push($vars, $up);
+        }
+        if ($right) {
+            array_push($vars, $right);
+        }
+        if ($down) {
+            array_push($vars, $down);
+        }
+        if ($left) {
+            array_push($vars, $left);
+        }
+        return $vars;
+    }
+    public function get_length()
+    {
+        $label = $this->END;
+        return $this->correct_way[$label->x][$label->y];
+    }
+    public function pathfind()
+    {
+        while ($this->stack->getLength() !== 0) {
+            $vars = $this->observe();
+            if (count($vars) === 0) {
+                $this->stack->remove();
+            } elseif (count($vars) === 1) {
+                $this->stack->moveCell($vars[0]);
+            } else {
+                $current = $this->stack->curr;
+                foreach ($vars as $new) {
+                    $this->stack->add($new, $current);
+                }
+            }
+        }
+    }
+
+    public function solution()
+    {
+        $this->pathfind();
+        if ($this->is_solved()) {
+            $label = new Cell($this->END->x, $this->END->y);
+            $this->correct_way[$label->x][$label->y] =
+                $this->paths[$label->x][$label->y];
+            $min = $this->paths[$label->x][$label->y];
+
+            while ($label != $this->START) {
+                $x = 0;
+                $y = 0;
+                $dirrection="";
+                if (
+                    $label->x + 1 < $this->HEIGHT &&
+                    isset($this->paths[$label->x + 1][$label->y])
+                ) {
+                    if ($this->paths[$label->x + 1][$label->y] < $min) {
+                        $min = $this->paths[$label->x + 1][$label->y];
+                        $x = 1;
+                        $y = 0;
+                        $dirrection="u";
+                    }
+                }
+                if (
+                    $label->x - 1 > -1 &&
+                    isset($this->paths[$label->x - 1][$label->y])
+                ) {
+                    if ($this->paths[$label->x - 1][$label->y] < $min) {
+                        $min = $this->paths[$label->x - 1][$label->y];
+                        $x = -1;
+                        $y = 0;
+                        $dirrection="d";
+                    }
+                }
+                if (
+                    $label->y + 1 < $this->LENGTH &&
+                    isset($this->paths[$label->x][$label->y + 1])
+                ) {
+                    if ($this->paths[$label->x][$label->y + 1] < $min) {
+                        $min = $this->paths[$label->x][$label->y + 1];
+                        $y = 1;
+                        $x = 0;
+                        $dirrection="l";
+                    }
+                }
+                if (
+                    $label->y - 1 > -1 &&
+                    isset($this->paths[$label->x][$label->y - 1])
+                ) {
+                    if ($this->paths[$label->x][$label->y - 1] < $min) {
+                        $min = $this->paths[$label->x][$label->y - 1];
+                        $y = -1;
+                        $x = 0;
+                        $dirrection="r";
+                    }
+                }
+                $label->x += $x;
+                $label->y += $y;
+                $this->compass_map=$dirrection.$this->compass_map;
+                $this->correct_way[$label->x][$label->y] =
+                    $this->paths[$label->x][$label->y];
+                //array_push($this->correct_way, new Cell($label->x, $label->y));
+            }
+        }
+    }
+    public function get_correct_way()
+    {
+        $corr = $this->correct_way;
+        $output = [];
+        for ($i = 0; $i < count($corr); $i++) {
+            $output[$i][0] = $corr[$i]->x;
+            $output[$i][1] = $corr[$i]->y;
+        }
+        return $output;
+    }
+    public function is_solved()
+    {
+        if ($this->paths[$this->END->x][$this->END->y]) {
+            return true;
         } else {
-            $current = $this->stack->curr;
-            foreach ($vars as $new) {
-                $this->stack->add($new, $current);
+            return false;
+        }
+    }
+    public function get_way_img($scale = 8)
+    {
+        $image = imagecreatetruecolor(
+            $this->HEIGHT * $scale,
+            $this->LENGTH * $scale
+        );
+
+        $white = imagecolorallocate($image, 255, 255, 255);
+        $black = imagecolorallocate($image, 0, 0, 0);
+        $green = imagecolorallocate($image, 0, 255, 0);
+
+        imagefill($image, 0, 0, $black);
+
+        for ($i = 0; $i < $this->HEIGHT; $i++) {
+            for ($j = 0; $j < $this->LENGTH; $j++) {
+                if ($this->maze[$i][$j] !== 0) {
+                    if ($this->correct_way[$i][$j]) {
+                        $color = $green;
+                    } else {
+                        $color = $white;
+                    }
+                    for ($x = $i * $scale; $x < $i * $scale + $scale; $x++) {
+                        for (
+                            $y = $j * $scale;
+                            $y < $j * $scale + $scale;
+                            $y++
+                        ) {
+                            imagesetpixel($image, $x, $y, $color);
+                        }
+                    }
+                }
             }
         }
-      }
+
+        $image = imagerotate($image, -90, 0);
+        imageflip($image, IMG_FLIP_HORIZONTAL);
+        imagepng($image, "img/solution.png");
+        imagedestroy($image);
     }
-
-  public function solution() {
-    $this->pathfind();
-    if ($this->is_solved()){
-    
-    $label = new Cell($this->END->x,$this->END->y);
-    $this->correct_way[$label->x][$label->y]=$this->paths[$label->x][$label->y];
-    $min = $this->paths[$label->x][$label->y];
-
-    while ($label != $this->START) {
-      $x = 0;
-      $y = 0;
-      if ($label->x + 1 < $this->HEIGHT && isset($this->paths[$label->x+1][$label->y])) {
-        if ($this->paths[$label->x+1][$label->y] < $min) {
-          $min = $this->paths[$label->x+1][$label->y];
-          $x = 1;
-          $y = 0;
-        }
-      }
-      if ($label->x - 1 > -1 && isset($this->paths[$label->x-1][$label->y])) {
-        if ($this->paths[$label->x-1][$label->y] < $min) {
-          $min = $this->paths[$label->x-1][$label->y];
-          $x = -1;
-          $y = 0;
-        }
-      }
-      if ($label->y + 1 < $this->LENGTH && isset($this->paths[$label->x][$label->y+1])) {
-        if ($this->paths[$label->x][$label->y+1] < $min) {
-          $min = $this->paths[$label->x][$label->y+1];
-          $y = 1;
-          $x = 0;
-        }
-      }
-      if ($label->y - 1 > -1 && isset($this->paths[$label->x][$label->y-1])) {
-        if ($this->paths[$label->x][$label->y-1] < $min) {
-          $min = $this->paths[$label->x][$label->y-1];
-          $y = -1;
-          $x = 0;
-        }
-      }
-      $label->x += $x;
-      $label->y += $y;
-      $this->correct_way[$label->x][$label->y]=$this->paths[$label->x][$label->y];
-      //array_push($this->correct_way, new Cell($label->x, $label->y));
-    }
-  }
-}
-  public function get_correct_way() {
-      $corr=$this->correct_way;
-      $output=array();
-      for($i=0; $i<count($corr);$i++){
-        $output[$i][0]=$corr[$i]->x;
-        $output[$i][1]=$corr[$i]->y;
-      }
-      return $output;
-  }
-  public function is_solved(){
-    if ($this->paths[$this->END->x][$this->END->y]){
-      return True;
-    }else{
-      return False;
-    }
-  }
-  public function get_way_img($scale=8){
-
-    $image = imagecreatetruecolor($this->HEIGHT*$scale, $this->LENGTH*$scale);
-
-    $white = imagecolorallocate($image, 255, 255, 255);
-    $black = imagecolorallocate($image, 0, 0, 0);
-    $green = imagecolorallocate($image, 0, 255, 0);
-
-
-    imagefill($image, 0, 0, $black);
-
-    for($i=0;$i<$this->HEIGHT;$i++){
-      for($j=0;$j<$this->LENGTH;$j++){
-        if ($this->maze[$i][$j]!==0){
-          if($this->correct_way[$i][$j]){
-            $color=$green;
-          }else{
-            $color=$white;
-          } 
-          for($x=$i*$scale;$x<$i*$scale+$scale;$x++){
-            for($y=$j*$scale;$y<$j*$scale+$scale;$y++){
-              imagesetpixel($image,$x,$y,$color);
-            }
-          }
-        }
-      }
-    }
-
-    $image=imagerotate($image, -90, 0);
-    imageflip($image, IMG_FLIP_HORIZONTAL);
-    // Отображаем изображение в браузере
-    imagepng($image,'solution.png');
-    // Освобождаем память, занимаемую изображением
-    imagedestroy($image);
-  }
 }
 
 ?>
